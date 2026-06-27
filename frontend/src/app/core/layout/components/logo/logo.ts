332bb09b05d9ea43933c87795f78d8b637dcb2ac
Gabriela Froń <gabriela.fron04@gmail.com>
2026-06-27 19:40:01 +0200

    migrate logo compoennt to tailwind

--- frontend/src/app/core/layout/components/logo/logo.ts
+++ frontend/src/app/core/layout/components/logo/logo.ts
@@ -1,14 +1,14 @@
-import { Component, computed, inject } from '@angular/core';
-import { SpotifyService } from '@src/app/spotify.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   imports: [],
   templateUrl: './logo.html',
-  styleUrl: './logo.scss',
+  host: {
+    class:
+      'grid h-full w-[min(35rem,100%)] min-w-[0] grid-cols-[6rem_minmax(0,auto)] items-center text-inherit [font-family:var(--default-mono-font-family)] max-[600px]:w-auto max-[600px]:grid-cols-[4.6rem_minmax(0,auto)] max-[360px]:grid-cols-[4rem]',
+  },
 })
 export class Logo {
-  readonly spotifyService = inject(SpotifyService);
-
   readonly logoUrl = 'logo.png';
 }
