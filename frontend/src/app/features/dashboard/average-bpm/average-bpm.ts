5b0f3960e358fb721c95957053aa3824db1abe78
Gabriela Froń <gabriela.fron04@gmail.com>
2026-06-27 18:55:48 +0200

    migrate average bpm component to tailwind

--- frontend/src/app/features/dashboard/average-bpm/average-bpm.ts
+++ frontend/src/app/features/dashboard/average-bpm/average-bpm.ts
@@ -5,7 +5,10 @@ import { Icon } from '@shared/components/icon/icon';
   selector: 'app-average-bpm',
   imports: [Icon],
   templateUrl: './average-bpm.html',
-  styleUrl: './average-bpm.scss',
+  host: {
+    class:
+      'row-start-1 row-end-2 col-start-2 col-end-3 grid min-h-[20rem] w-full min-w-[0] grid-cols-1 grid-rows-[auto_minmax(10rem,1fr)] gap-[1rem] max-[960px]:min-h-[16rem] max-[960px]:grid-rows-[auto_minmax(7rem,1fr)] max-[600px]:min-h-[14rem]',
+  },
 })
 export class AverageBpm {
   public readonly averageBpm = input<number | null>(null);
